bad1d9c4515d4cb9bb938dcddd34a3d882d654d5
Sérgio Ribeiro <sergio.m.rib@gmail.com>
2021-02-06 18:01:11 +0000

    Player is instantiated with weapons

--- src/org/academiadecodigo/bootcamp/civilwar/gameobject/player/Player.java
+++ src/org/academiadecodigo/bootcamp/civilwar/gameobject/player/Player.java
@@ -5,21 +5,35 @@ import org.academiadecodigo.bootcamp.civilwar.gameobject.objinterface.Destroyabl
 import org.academiadecodigo.bootcamp.civilwar.gameobject.position.Direction;
 import org.academiadecodigo.bootcamp.civilwar.gameobject.position.Position;
 import org.academiadecodigo.bootcamp.civilwar.gameobject.weapon.Weapon;
+import org.academiadecodigo.bootcamp.civilwar.gameobject.weapon.WeaponType;
 
 public class Player extends GameObject implements PlayerInterface, Destroyable {
     private boolean destroyed = false;
     private int health;
     private final int speed;
     private Direction direction;
+    private Weapon solidWeapon;
+    private Weapon liquidWeapon;
 
 
-    public Player(Position myPos){
+    public Player(Position myPos, String player){
         super(myPos);
         this.health = 10;
         this.speed = 5;
+
+        // TO CHANGE
+        if(player.equals("SéDude")){
+            //solidWeapon = WeaponType.FRANCESINHA;
+            //liquidWeapon = WeaponType.VINHODOPORTO;
+        } else if(player.equals("AlfamaDude")) {
+            //solidWeapon = WeaponType.PASTELDEBELEM;
+            //liquidWeapon = WeaponType.GINJA;
+        }
+
     }
 
     public void move(Direction direction){
+        this.direction = direction;
         getPosition().moveInDirection(direction, speed);
     }
 
